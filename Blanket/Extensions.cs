a940a6a23ab44d4f60a86436c0326e62bb94df18
shadowninja108 <shadowninja0108@gmail.com>
2020-03-17 20:06:42 -0700

    Add project files.

---
+++ Blanket/Extensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Blanket
+{
+    static class Extensions
+    {
+        public static FileInfo GetFile(this DirectoryInfo obj, string filename)
+        {
+            return new FileInfo($"{obj.FullName}{Path.DirectorySeparatorChar}{filename}");
+        }
+
+        public static bool ContainsFile(this DirectoryInfo obj, string filename)
+        {
+            return obj.GetFile(filename).Exists;
+        }
+
+        public static DirectoryInfo GetDirectory(this DirectoryInfo obj, string foldername)
+        {
+            return new DirectoryInfo($"{obj.FullName}{Path.DirectorySeparatorChar}{foldername.Replace('/', Path.DirectorySeparatorChar)}");
+        }
+
+        public static byte[] ReadAll(this FileInfo obj)
+        {
+            byte[] buff = new byte[obj.Length];
+            using (FileStream stream = obj.OpenRead())
+                stream.Read(buff);
+            return buff;
+        }
+
+        public static void WriteAll(this FileInfo obj, byte[] data)
+        {
+            if (!obj.Exists)
+                using (obj.Create());
+            using FileStream stream = obj.OpenWrite();
+            stream.Write(data, 0, data.Length);
+        }
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> e, int split, int count)
+        {
+            for(int i = 0; i < count; i++)
+                yield return e.Skip(i * split).Take(split);
+        }
+
+
+        public static uint ToUInt32(this IEnumerable<byte> bytes)
+        {
+            return BitConverter.ToUInt32(bytes.ToArray(), 0);
+        }
+
+
+        private static readonly uint[] Lookup32 = CreateLookup32();
+        private static uint[] CreateLookup32()
+        {
+            var result = new uint[256];
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("X2");
+                result[i] = s[0] + ((uint)s[1] << 16);
+            }
+            return result;
+        }
+
+        public static string ToHexString(this IEnumerable<byte> bytes) => ToHexString(bytes.ToArray());
+        public static string ToHexString(this byte[] bytes) => ToHexString(bytes.AsSpan());
+        public static string ToHexString(this Span<byte> bytes)
+        {
+            uint[] lookup32 = Lookup32;
+            var result = new char[bytes.Length * 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                uint val = lookup32[bytes[i]];
+                result[2 * i] = (char)val;
+                result[2 * i + 1] = (char)(val >> 16);
+            }
+            return new string(result);
+        }
+
+    }
+}
---
+++ Blanket/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Blanket
+{
+    class Program
+    {
+        static uint[] ImportantSection;
+
+        static readonly string[] Paths = new string[]
+        {
+            "main",
+            "Villager0/personal",
+            "Villager0/photo_studio_island",
+            "Villager0/postbox",
+            "Villager0/profile"
+        };
+        static void Main(string[] args)
+        {
+            string root = "B:/Downloads/";
+            DirectoryInfo rootDir = new DirectoryInfo(root);
+
+            foreach(string f in Paths)
+            {
+
+                FileInfo file = rootDir.GetFile($"{f}.dat");
+                if (!file.Exists)
+                    continue;
+
+                Console.WriteLine($"Decrypting {f}...");
+                FileInfo header = rootDir.GetFile($"{f}Header.dat");
+
+                byte[] headerData = header.ReadAll();
+                int importantSectionCount = (headerData.Length - 0x100) / sizeof(uint);
+                ImportantSection = headerData.Skip(0x100)
+                    .Split(sizeof(uint), importantSectionCount)
+                    .Select(x => x.ToUInt32()).ToArray();
+
+                byte[] key = GenKey(0);
+                Console.WriteLine($"Key: {key.ToHexString()}");
+                byte[] iv = GenKey(2);
+                Console.WriteLine($"IV: {iv.ToHexString()}");
+
+                byte[] fileData = file.ReadAll();
+                byte[] dec = Decrypt(fileData, key, iv);
+                rootDir.GetFile($"{f}.dec").WriteAll(dec);
+            }
+        }
+
+        static byte[] GenKey(int s)
+        {
+            uint tickCount = ImportantSection[ImportantSection[s + 1] & 0x7F] & 0xF;
+            tickCount++;
+            SeadRandom random = new SeadRandom(ImportantSection[ImportantSection[s] & 0x7F]);
+            for (int i = 0; i < tickCount; i++)
+                random.GetU64();
+
+            byte[] key = new byte[0x10];
+            for(int i = 0; i < 0x10; i++)
+                key[i] = (byte)((random.GetU32() >> 24) & 0xFF);
+
+            return key;
+        }
+
+        static byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            Aes128CounterMode am = new Aes128CounterMode(iv);
+            ICryptoTransform ict = am.CreateDecryptor(key, iv);
+            byte[] dec = new byte[data.Length];
+            ict.TransformBlock(data, 0, data.Length, dec, 0);
+            return dec;
+        }
+    }
+}
